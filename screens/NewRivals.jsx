b5597f4b81e73945c8f6e254c83accb15deb629d
Fujiwara0805 <tiki4091@gmail.com>
2024-02-04 21:37:59 +0900

    ✨ NewRivalsコンポーネントの追加

---
+++ screens/NewRivals.jsx
@@ -0,0 +1,31 @@
+import { View, TouchableOpacity, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons'
+import styles from './NewRivals.style';
+import { COLORS } from '../constants';
+
+const NewRivals = ({navigation}) => {
+  const navigateBack = () => {
+    navigation.goBack()
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <View style={styles.upperRow}>
+          <TouchableOpacity onPress={navigateBack}>
+            <Ionicons 
+              name='chevron-back-circle' 
+              size={30}
+              color={COLORS.lightWhite}
+            />
+          </TouchableOpacity>
+          <Text>Products</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default NewRivals
